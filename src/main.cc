ae1d1c9d0b5abe29be54f1347bf7894dc7a59f92
ucev <zhangshuaiyf@icloud.com>
2017-09-22 09:56:54 +0800

    add new line after entering password

--- src/main.cc
+++ src/main.cc
@@ -48,8 +48,49 @@ namespace _getpass {
     // not used
     int error;
     string prompt;
-    string passwd;
+    char *passwd;
   };
+
+  void ReadAllocCallback(uv_handle_t*, size_t, uv_buf_t*);
+  void ReadReadCallback(uv_stream_t*, ssize_t, const uv_buf_t*);
+  void WriteNewLineCallback(uv_write_t*, int);
+  void WritePromptCallback(uv_write_t*, int);
+
+  void Delay(const FunctionCallbackInfo<Value>& args) {
+    Isolate *isolate = args.GetIsolate();
+    Local<Context> context = isolate->GetCurrentContext();
+    DelayBaton *baton = new DelayBaton;
+    uv_loop_t *loop = uv_default_loop();
+    int cbIndex = 0;
+    string prompt = "请输入密码：";
+    if (!args[0]->IsFunction()) {
+      cbIndex = 1;
+      if (args[0]->IsObject()) {
+        Local<Object> obj = args[0]->ToObject();
+        Local<String> key = String::NewFromUtf8(isolate, "prompt");
+        Maybe<bool> maybeHas = obj->Has(context, key);
+        if (maybeHas.IsJust()) {
+          String::Utf8Value sval(obj->Get(context, key).ToLocalChecked());
+          prompt.assign(*sval);
+        }
+      }
+    }
+    if (!args[cbIndex]->IsFunction()) {
+      isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "参数错误")));
+      delete baton;
+      return;
+    }
+    Local<Function> cb = Local<Function>::Cast(args[cbIndex]);
+    baton->callback.Reset(isolate, cb);
+    baton->error = 0;
+    set_disp_mode(STDIN_FILENO, 0);
+    uv_buf_t buf;
+    buf.base = (char*)prompt.c_str();
+    buf.len = strlen(buf.base);
+    uv_tty_init(loop, &baton->tty_w, 1, 0);
+    baton->write.data = baton;
+    uv_write(&baton->write, (uv_stream_t*)&baton->tty_w, &buf, 1, WritePromptCallback);
+  }
 
   void ReadAllocCallback(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
     //
@@ -69,54 +110,43 @@ namespace _getpass {
     HandleScope handleScope(isolate);
     uv_read_stop(stream);
     DelayBaton *baton = static_cast<DelayBaton*>(stream->data);
-    const int argc = 2;
-    Local<Value> argv[] = {
-      Undefined(isolate),
-      Local<Value>(String::NewFromUtf8(isolate, buf->base))
-    };
-    set_disp_mode(STDIN_FILENO, 1);
-    Local<Function>::New(isolate, baton->callback)->Call(isolate->GetCurrentContext()->Global(), argc, argv);
-    baton->callback.Reset();
-    delete baton;
+    baton->passwd = buf->base;
+    string newLine = "\n";
+    uv_buf_t wbuf;
+    wbuf.base = (char*)newLine.c_str();
+    wbuf.len = strlen(wbuf.base);
+    uv_write(&baton->write, (uv_stream_t*)&baton->tty_w, &wbuf, 1, WriteNewLineCallback);
   }
 
-  void Delay(const FunctionCallbackInfo<Value>& args) {
-    Isolate *isolate = args.GetIsolate();
-    Local<Context> context = isolate->GetCurrentContext();
-    DelayBaton *baton = new DelayBaton;
-    uv_loop_t *loop = uv_default_loop();
-    int cbIndex = 0;
-    string prompt = "请输入密码";
-    if (!args[0]->IsFunction()) {
-      cbIndex = 1;
-      if (args[0]->IsObject()) {
-        Local<Object> obj = args[0]->ToObject();
-        Local<String> key = String::NewFromUtf8(isolate, "prompt");
-        Maybe<bool> maybeHas = obj->Has(context, key);
-        if (maybeHas.IsJust()) {
-          String::Utf8Value sval(obj->Get(context, key).ToLocalChecked());
-          prompt.assign(*sval);
-        }
-      }
-    }
-    if (!args[cbIndex]->IsFunction()) {
-      isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "参数错误")));
+  void WriteNewLineCallback(uv_write_t *req, int status) {
+    // no error handler at current time
+    if (status == 0) {
+      Isolate *isolate = Isolate::GetCurrent();
+      HandleScope handleScope(isolate);
+      DelayBaton *baton = static_cast<DelayBaton*>(req->data);
+      const int argc = 2;
+      Local<Value> argv[] = {
+        Undefined(isolate),
+        Local<Value>(String::NewFromUtf8(isolate, baton->passwd))
+      };
+      set_disp_mode(STDIN_FILENO, 1);
+      Local<Function>::New(isolate, baton->callback)->Call(isolate->GetCurrentContext()->Global(),argc, argv);
+      baton->callback.Reset();
       delete baton;
-      return;
     }
-    //baton->prompt.assign(prompt, 0, prompt.length());
-    Local<Function> cb = Local<Function>::Cast(args[cbIndex]);
-    baton->callback.Reset(isolate, cb);
-    baton->error = 0;
-    set_disp_mode(STDIN_FILENO, 0);
-    uv_buf_t buf;
-    buf.base = (char*)prompt.c_str();
-    buf.len = strlen(buf.base);
-    uv_tty_init(loop, &baton->tty_w, 1, 0);
-    uv_write(&baton->write, (uv_stream_t*)&baton->tty_w, &buf, 1, NULL);
-    uv_tty_init(loop, &baton->tty, 0, 1);
-    baton->tty.data = baton;
-    uv_read_start((uv_stream_t*)&baton->tty, ReadAllocCallback, ReadReadCallback);
+  }
+
+  void WritePromptCallback(uv_write_t *req, int status) {
+    // no error handler at current time
+    if (status == 0) {
+      Isolate *isolate = Isolate::GetCurrent();
+      HandleScope handleScope(isolate);
+      DelayBaton *baton = static_cast<DelayBaton*>(req->data);
+      uv_loop_t *loop = uv_default_loop();
+      uv_tty_init(loop, &baton->tty, 0, 1);
+      baton->tty.data = baton;
+      uv_read_start((uv_stream_t*)&baton->tty, ReadAllocCallback, ReadReadCallback);
+    }
   }
 
   void init(Local<Object> exports) {
--- test/getpass.js
+++ test/getpass.js
@@ -1,8 +1,6 @@
-const getpass = require('../index.js');
-getpass.getpass({prompt: "请输入密码"}).then((pass) => {
-  console.log(`你输入的密码是: ${pass}`);
-}).catch((e) => {
-  console.log("ERROR");
+const getpass = require('../index.js')
+getpass.getpass({prompt: "请输入密码："}).then((pass) => {
+  console.log(`你输入的密码是: ${pass}`)
+}).catch(e => {
+  console.log(e)
 });
-var a = 1, b = 2;
-console.log(`${a} + ${b} = ${a + b}`);
